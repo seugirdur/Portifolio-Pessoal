ea9e3a284978306c23cbc040ffcf37939d214b15
seugirdur <gabriel.lima137170@gmail.com>
2025-01-13 21:27:34 -0300

    Configuração para GitHub Pages

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,70 +1,92 @@
-import {useEffect, useState} from "react";
+import { useTranslation } from "react-i18next";
+
+interface Project {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+    technologies: string[];
+}
 
 interface ProjectCardProps {
-  key: number;
+    projectKey: number;
 }
-export const ProjectCard = ({ projectKey }: { projectKey: number }) => {
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
-    const [image, setImage] = useState("")
-    const [technologies, setTechnologies] = useState([])
-    const [link, setLink] = useState("")
-    useEffect(() => {
-        switch (projectKey) {
-            case 1:
-                setTitle("ASET");
-                setDescription("ASET foi uma aplicação Android desenvolvida para a Petrobras para centralizar os pedidos de ajuda de suporte técnico.");
-                setImage("https://github.com/seugirdur/ASET/blob/main/img/slide/quatro.png?raw=true")
-                setLink("https://github.com/seugirdur/App-Petrobras")
-                break;
-            case 2:
-                setTitle("Powdermix");
-                setDescription("E-commerce de maquinário para farmácias.");
-                setLink("https://github.com/seugirdur/App-Powdermix")
-                setImage("https://powdermix.com.br/wp-content/uploads/2024/07/Logo_Novo_PowderMix_Colorido_v2-300x73.png")
-                break;
-            case 3:
-                setTitle("FX BANK");
-                setDescription("Página de Checkout Customizado com Dashboard para análise de vendas, integração com Shopify, PagSeguro, Safe2Pay e outros.");
-                setLink("https://github.com/seugirdur/fx-bank")
-                setImage("https://fxbank.com.br/wp-content/uploads/2024/08/fx-1.png")
-                break;
-            default:
-                break;
-        }
-    }, [projectKey]);
 
-  return (
-    <div className="m-2 lg:pb-2 pb-2 lg:w-72 w-[70%] lg:justify-start justify-center items-center flex flex-col lg:ring-4 rounded-3xl lg:ring-lightblue-portfolio bg-lightblue-portfolio bg-opacity-20 tracking-widest lg:shadow-none shadow-2xl"
-    onClick={() => window.open(link, "_blank")}>
+const projects: Record<number, Project> = {
+    1: {
+        title: "ASET",
+        description:
+            "ASET foi uma aplicação Android desenvolvida para a Petrobras para centralizar os pedidos de ajuda de suporte técnico.",
+        image:
+            "https://github.com/seugirdur/ASET/blob/main/img/slide/quatro.png?raw=true",
+        link: "https://github.com/seugirdur/App-Petrobras",
+        technologies: [
+            "https://cdn-icons-png.flaticon.com/512/5968/5968282.png",
+            "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png",
+        ],
+    },
+    2: {
+        title: "Powdermix",
+        description: "E-commerce de maquinário para farmácias.",
+        image:
+            "https://powdermix.com.br/wp-content/uploads/2024/07/Logo_Novo_PowderMix_Colorido_v2-300x73.png",
+        link: "https://github.com/seugirdur/App-Powdermix",
+        technologies: [
+            "https://cdn-icons-png.flaticon.com/512/5968/5968282.png",
+            "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png",
+        ],
+    },
+    3: {
+        title: "FX BANK",
+        description:
+            "Página de Checkout Customizado com Dashboard para análise de vendas, integração com Shopify, PagSeguro, Safe2Pay e outros.",
+        image:
+            "https://fxbank.com.br/wp-content/uploads/2024/08/fx-1.png",
+        link: "https://github.com/seugirdur/fx-bank",
+        technologies: [
+            "https://cdn-icons-png.flaticon.com/512/5968/5968282.png",
+            "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png",
+        ],
+    },
+};
 
-      <div className="flex flex-col bg-blue-portfolio lg:w-72 w-full py-1 rounded-3xl">
-          <img
-              src={image}
-              alt="project logo"
-              className={`h-[235px] w-auto rounded-3xl ${
-                  projectKey === 2 ? "object-contain" : "object-cover"
-              }`}
-          /></div>
-        <h1 className="flex flex-col items-start justify-start text-start mr-auto lg:text-xl text-3xl text-lightblue-portfolio font-bold p-2">
-          {title}
-      </h1>
-      <p className="lg:text-base text-lg p-2 tracking-widest">
-          {description}
-      </p>
+export const ProjectCard = ({ projectKey }: ProjectCardProps) => {
+    const { t } = useTranslation(); // Hook para tradução
+    const project = projects[projectKey];
 
-      <div className="flex flex-row justify-center">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/5968/5968282.png"
-          alt=""
-          className="p-2 w-12 h-12"
-        />
-        <img
-          src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png"
-          alt=""
-          className="p-2  w-12 h-12"
-        />
-      </div>
-    </div>
-  );
+    if (!project) return null; // Retorna null caso o projeto não seja encontrado
+
+    return (
+        <div
+            className="m-2 lg:pb-2 pb-2 lg:w-72 w-[70%] lg:justify-start justify-center items-center flex flex-col lg:ring-4 rounded-3xl lg:ring-lightblue-portfolio bg-lightblue-portfolio bg-opacity-20 tracking-widest lg:shadow-none shadow-2xl"
+            onClick={() => window.open(project.link, "_blank")}
+        >
+            <div className="flex flex-col bg-blue-portfolio lg:w-72 w-full py-1 rounded-3xl">
+                <img
+                    src={project.image}
+                    alt="project logo"
+                    className={`h-[235px] w-auto rounded-3xl ${
+                        projectKey === 2 ? "object-contain" : "object-cover"
+                    }`}
+                />
+            </div>
+            <h1 className="flex flex-col items-start justify-start text-start mr-auto lg:text-xl text-3xl text-lightblue-portfolio font-bold p-2">
+                {t(`projects_card.${projectKey}.title`, project.title)}
+            </h1>
+            <p className="lg:text-base text-lg p-2 tracking-widest">
+                {t(`projects_card.${projectKey}.description`, project.description)}
+            </p>
+
+            <div className="flex flex-row justify-center">
+                {project.technologies.map((tech, index) => (
+                    <img
+                        key={index}
+                        src={tech}
+                        alt={`tech-${index}`}
+                        className="p-2 w-12 h-12"
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
--- src/pages/Servicos.tsx
+++ src/pages/Servicos.tsx
@@ -1,105 +1,94 @@
 import IdeaIcon from "../assets/idea.png";
 import Medicine from "../assets/alternative-medicine.png";
+import { useTranslation } from "react-i18next";
+
+const technologies = [
+  {
+    id: "java",
+    img: "https://cdn-icons-png.flaticon.com/512/5968/5968282.png",
+  },
+  {
+    id: "node",
+    img: "https://nodejs.org/static/logos/jsIconGreen.svg",
+  },
+  {
+    id: "python",
+    img: "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png",
+  },
+  {
+    id: "react",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    id: "vue",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png",
+  },
+  {
+    id: "reactNative",
+    img: "https://cdn.worldvectorlogo.com/logos/react-native-1.svg",
+  },
+];
 
 export const Servicos = () => {
+  const { t } = useTranslation(); // Hook para tradução
+
   return (
-    <>
-      <div
-        id="servico"
-        className="relative h-screen font-Poppins z-10 text-white "
-      >
-        <div className="absolute z-0 mt-20 flex-col h-screen w-screen lg:hidden flex overflow-hidden">
-          <div className=" justify-center flex-col lg:hidden flex lg:right-0  w-screen ">
-            <div className="w-[40vw] bubble h-[40vw] flex flex-col  bg-gradient-to-r from-transparent to-lightblue-portfolio rounded-full opacity-20 mr-auto -ml-12 shadow-black shadow-xl mt-24"></div>
+      <>
+        <div
+            id="servico"
+            className="relative h-screen font-Poppins z-10 text-white "
+        >
+          <div className="absolute z-0 mt-20 flex-col h-screen w-screen lg:hidden flex overflow-hidden">
+            <div className="justify-center flex-col lg:hidden flex lg:right-0 w-screen">
+              <div className="w-[40vw] bubble h-[40vw] flex flex-col bg-gradient-to-r from-transparent to-lightblue-portfolio rounded-full opacity-20 mr-auto -ml-12 shadow-black shadow-xl mt-24"></div>
+            </div>
+            <div className="w-[40vw] bubble h-[40vw] flex flex-col bg-gradient-to-t from-transparent to-lightblue-portfolio rounded-full opacity-20 ml-auto -mr-12 mb-12 -mt-12 shadow-black"></div>
+            <div className="w-[90vw] bubble h-[90vw] flex flex-col bg-gradient-to-tl from-transparent to-lightblue-portfolio rounded-full opacity-20 -ml-12 mb-32 mt-auto"></div>
           </div>
-          <div className="w-[40vw] bubble h-[40vw] flex flex-col bg-gradient-to-t from-transparent to-lightblue-portfolio rounded-full opacity-20 ml-auto -mr-12 mb-12 -mt-12 shadow-black "></div>
-          <div className="w-[90vw] bubble h-[90vw] flex flex-col bg-gradient-to-tl from-transparent to-lightblue-portfolio rounded-full  opacity-20 -ml-12 mb-32 mt-auto"></div>
+          <div className="w-full flex flex-col items-center text-center justify-center lg:p-16 p-8 py-12 pb-6 lg:text-6xl text-3xl">
+            <h1 className="font-bold">
+              {t("services.title")}
+              <span className="text-lightblue-portfolio">.</span>
+            </h1>
+          </div>
+          <div className="justify-evenly flex flex-wrap flex-row tracking-widest">
+            {technologies.map((tech) => (
+                <div className="flex flex-col p-2 items-center" key={tech.id}>
+                  <img
+                      src={tech.img}
+                      alt={t(`services.technologies.${tech.id}`)}
+                      className="p-2 lg:w-36 lg:h-36 w-24 h-24"
+                  />
+                  <p className="lg:text-2xl text-lg p-2 uppercase">
+                    {t(`services.technologies.${tech.id}`)}
+                  </p>
+                </div>
+            ))}
+          </div>
+          <div className="flex lg:flex-row flex-col justify-evenly lg:px-20 tracking-widest">
+            <div className="flex flex-col items-center lg:w-1/2 justify-start text-center lg:p-4 lg:px-20 px-6">
+              <img src={Medicine} className="mb-4 lg:flex hidden h-32" />
+              <img src={Medicine} className="lg:mb-4 lg:hidden flex h-16" />
+              <h2 className="lg:text-4xl text-xl text-lightblue-portfolio font-bold uppercase m-2 text-center">
+                {t("services.development.title")}
+              </h2>
+              <p className="lg:text-2xl text-lg lg:mb-0 mb-4 uppercase lg:leading-normal leading-none">
+                {t("services.development.description")}
+              </p>
+            </div>
+            <div className="flex flex-col items-center lg:w-1/2 justify-start text-center lg:p-4 lg:px-20 px-6">
+              <img src={IdeaIcon} className="mb-4 lg:flex hidden h-32" />
+              <img src={IdeaIcon} className="lg:mb-4 lg:hidden flex h-16 scaleAni animation-3s" />
+              <h2 className="lg:text-4xl text-xl text-lightblue-portfolio font-bold uppercase m-2 text-center">
+                {t("services.branding.title")}
+              </h2>
+              <p className="lg:text-2xl text-lg uppercase text-center lg:leading-normal leading-none">
+                {t("services.branding.description")}
+              </p>
+            </div>
+          </div>
+          <div className="w-[35vw] h-[35vw] bubble lg:flex hidden flex-col rounded-full bg-gradient-to-b from-baby-blue to-blue-portfolio relative opacity-5 shadow-black shadow-xl top-0 mt-[-120vh] ml-[20vw] left-[0]"></div>
         </div>
-        <div className="w-full flex flex-col items-center text-center justify-center lg:p-16 p-8 py-12 pb-6 lg:text-6xl text-3xl">
-          <h1 className="font-bold">
-            Conheça um pouco do meu trabalho
-            <span className="text-lightblue-portfolio">.</span>
-          </h1>
-        </div>
-        <div className="justify-evenly flex flex-wrap flex-row tracking-widest">
-          <div className="flex flex-col p-2 items-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/5968/5968282.png"
-              alt=""
-              className="p-2 lg:w-36 lg:h-36 w-24 h-24"
-            />
-            <p className="lg:text-2xl text-lg p-2 uppercase">Java</p>
-          </div>
-          <div className="flex flex-col p-2 items-center">
-            <img
-              src="https://nodejs.org/static/logos/jsIconGreen.svg"
-              alt=""
-              className="p-2 lg:w-36 lg:h-36 w-24 h-24"
-            />
-            <p className="lg:text-2xl text-lg p-2 uppercase">Node.js</p>
-          </div>
-          <div className="flex flex-col p-2 items-center">
-            <img
-              src="https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png"
-              alt=""
-              className="p-4 lg:w-36 lg:h-36 w-24 h-24"
-            />
-            <p className="lg:text-2xl text-lg p-2 uppercase">Python</p>
-          </div>
-          <div className="flex flex-col p-2 items-center">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
-              alt=""
-              className="p-4 py-5 lg:w-36 lg:h-36 w-24 h-24"
-            />
-            <p className="lg:text-2xl text-lg p-2 uppercase">React</p>
-          </div>
-          <div className="flex flex-col p-2 items-center">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png"
-              alt=""
-              className="p-4 lg:w-36 lg:h-36 w-24 h-24"
-            />
-            <p className="lg:text-2xl text-lg p-2 uppercase">Vue.js</p>
-          </div>
-          <div className="flex flex-col p-2 items-center">
-            <img
-              src="https://cdn.worldvectorlogo.com/logos/react-native-1.svg"
-              alt=""
-              className="p-2 lg:w-36 lg:h-36 w-24 h-24"
-            />
-            <p className="lg:text-2xl text-lg p-2 uppercase">React<br/>Native</p>
-          </div>
-        </div>
-        <div className="flex lg:flex-row flex-col justify-evenly lg:px-20 tracking-widest">
-          <div className="flex flex-col items-center lg:w-1/2 justify-start text-center lg:p-4 lg:px-20 px-6">
-            <img src={Medicine} className="mb-4 lg:flex hidden h-32" />
-            <img src={Medicine} className="lg:mb-4 lg:hidden flex h-16" />
-            <h2 className="lg:text-4xl text-xl text-lightblue-portfolio font-bold uppercase m-2 text-center">
-              Desenvolvimento
-            </h2>
-            <p className="lg:text-2xl text-lg lg:mb-0 mb-4 uppercase lg:leading-normal leading-none">
-              Criação de sistemas feitos exclusivamente para você, com o
-              propósito de automatizar a gestão empresarial.
-            </p>
-          </div>
-          <div className="flex flex-col items-center lg:w-1/2 justify-start text-center lg:p-4 lg:px-20 px-6">
-            <img src={IdeaIcon} className="mb-4 lg:flex hidden h-32" />
-            <img
-              src={IdeaIcon}
-              className="lg:mb-4 lg:hidden flex h-16 scaleAni animation-3s"
-            />
-            <h2 className="lg:text-4xl text-xl text-lightblue-portfolio font-bold uppercase m-2 text-center">
-              Branding
-            </h2>
-            <p className="lg:text-2xl text-lg uppercase text-center lg:leading-normal leading-none">
-              auxiliar você a estrategicamente posicionar sua marca,
-              impulsionando resultados positivos
-            </p>
-          </div>
-        </div>
-        <div className="w-[35vw] h-[35vw] bubble lg:flex hidden flex-col rounded-full bg-gradient-to-b from-baby-blue to-blue-portfolio relative opacity-5 shadow-black shadow-xl top-0 mt-[-120vh] ml-[20vw] left-[0]"></div>
-      </div>
-    </>
+      </>
   );
 };
