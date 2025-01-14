a14b88af39f3cbd39c38ad386b39c7ddab01c00d
seugirdur <gabriel.lima137170@gmail.com>
2025-01-13 22:06:41 -0300

    arrumando o app config

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,24 @@
-import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Projetos } from "./pages/Projetos";
 import { Servicos } from "./pages/Servicos";
-import { Events, scrollSpy } from "react-scroll";
 import { Processos } from "./pages/Processos";
 import { Experiencias } from "./pages/Experiencias";
-import { Desenvolvedor } from "./pages/Desenvolvedor.tsx";
+import { Desenvolvedor } from "./pages/Desenvolvedor";
 import { Contato } from "./pages/Contato";
 
 export const App = () => {
-  useEffect(() => {
-    Events.scrollEvent.register("begin", (element) => {
-      console.log(`Scrolling to: ${element}`);
-    });
-
-    scrollSpy.update();
-
-    return () => {
-      Events.scrollEvent.remove("begin");
-    };
-  }, []);
-
-  return (
-      <div className="h-full overflow-hidden">
-        <section id="home">
-          <Home />
-        </section>
-        <section id="projetos">
-          <Projetos />
-        </section>
-        <section id="servicos">
-          <Servicos />
-        </section>
-        <section id="processos">
-          <Processos />
-        </section>
-        <section id="desenvolvedor">
-          <Desenvolvedor />
-        </section>
-        <section id="experiencias">
-          <Experiencias />
-        </section>
-        <section id="contato">
-          <Contato />
-        </section>
-      </div>
-  );
+    return (
+        <BrowserRouter basename="/Portifolio-Pessoal"> {/* Configura o subdiretório */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projetos" element={<Projetos />} />
+                <Route path="/servicos" element={<Servicos />} />
+                <Route path="/processos" element={<Processos />} />
+                <Route path="/desenvolvedor" element={<Desenvolvedor />} />
+                <Route path="/experiencias" element={<Experiencias />} />
+                <Route path="/contato" element={<Contato />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
