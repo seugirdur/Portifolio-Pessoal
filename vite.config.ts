b84a3ae16074419bdb33c580dc66fabdca0420b8
seugirdur <gabriel.lima137170@gmail.com>
2025-01-13 21:53:59 -0300

    arrumando config do vite para path relativo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Portifolio-Pessoal/", // Caminho base do repositório
+});
