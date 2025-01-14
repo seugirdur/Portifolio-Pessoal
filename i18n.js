01adeb07de1b28be4c9aaca6b12d7c3a1d135b26
seugirdur <gabriel.lima137170@gmail.com>
2025-01-13 22:05:24 -0300

    testando mexer na config de tradução

--- i18n.js
+++ i18n.js
@@ -1,21 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import HttpApi from "i18next-http-backend";
 
-// Configuração do i18n
 i18n
-    .use(Backend) // Para carregar traduções de arquivos JSON
-    .use(LanguageDetector) // Detecta o idioma do navegador
-    .use(initReactI18next) // Integração com React
+    .use(initReactI18next)
+    .use(HttpApi)
     .init({
-        fallbackLng: "en", // Idioma padrão
-        debug: true, // Ativa logs para identificar erros
-        interpolation: {
-            escapeValue: false, // React já faz proteção contra XSS
-        },
+        lng: "en", // Idioma padrão
+        fallbackLng: "en",
+        debug: true,
         backend: {
-            loadPath: "/locales/{{lng}}/translation.json", // Caminho para os arquivos JSON
+            loadPath: "/Portifolio-Pessoal/locales/{{lng}}/{{ns}}.json", // Inclua o subdiretório
+        },
+        interpolation: {
+            escapeValue: false,
         },
     });
 
